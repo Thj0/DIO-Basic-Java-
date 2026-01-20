8e2096f299fb6f31eebe056ebf3b60383b1a5504
Thj0 <jhnnt.13@gmail.com>
2026-01-20 19:34:23 -0300

    Matriz

--- Arrays/src/consoantes.java
+++ Arrays/src/consoantes.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class consoantes {
     public static void main(String[] args) {
+        Scanner look = new Scanner(System.in);
+
         
-        
+
+
     }
     
 }
