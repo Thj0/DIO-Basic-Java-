2b08928a02365a11fba225b14417f3c9d17fcf72
Thj0 <jhnnt.13@gmail.com>
2026-02-09 17:01:23 -0300

    Independente

---
+++ OperaçõesMatematicas/OperacaoMatematica.java
@@ -0,0 +1,18 @@
+package OperaçõesMatematicas;
+
+/*Interface de exemplo para o exercício de Orientação a Objetos. */
+
+public interface OperacaoMatematica {
+    
+    void soma(double operando1, double operando2);
+
+
+    void subtracao(double operando1, double operando2);
+
+
+    void multiplicacao(double operando1, double operando2);
+
+    
+    void divisao(double operando1, double operando2); 
+
+}
