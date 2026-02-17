9844aee43ddd69b4378c4d4497ee38c7fd16c277
Thj0 <jhnnt.13@gmail.com>
2026-02-17 16:35:34 -0300

    Tempo

--- List/Temperatura.java
+++ List/Temperatura.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.Collections;
 
 
-public class Temperatura {
+public class Temperatura { //Inicio da classe principal do exercício.
     public static void main(String[] args) 
     {
     
-        List<Mes> primeiroSemestre = new ArrayList<>()
+        List<Mes> primeiroSemestre = new ArrayList<>() // Onde começo a lista principal,  que guarda as informaçoes dos meses e suas temperaturas.
         {{
 
             add(new Mes(1, "Janeiro", 28));
@@ -22,24 +22,24 @@ public class Temperatura {
         }};
         System.out.print(primeiroSemestre);
 
-        double soma = 0;
+        double soma = 0; // Variável que serve como controle, é um contador, porém, com a utilizade de guardar os valores das temperaturas e somá-los. 
 
 
-        for(int i = 0; i < primeiroSemestre.size(); i++)
+        for(int i = 0; i < primeiroSemestre.size(); i++) //Função de controle, onde o i é utilizado para que percorra dentro da minha list e retorne as temperaturas.
         {
-            soma += primeiroSemestre.get(i).getTemperatura();
+            soma += primeiroSemestre.get(i).getTemperatura(); // 'soma' recebe as temperaturas da list e armazena dentro da variável.
         }
 
 
         
-        double media = soma / primeiroSemestre.size();
+        double media = soma / primeiroSemestre.size();  // media recebe os valores de soma e divide pelo tamanho de list.
 
 
 
-        for(int i = 0; i < primeiroSemestre.size(); i++)
+        for(int i = 0; i < primeiroSemestre.size(); i++) //Enquanto o índice de i for menor que o tamanho de list, percorra.
         {
             
-            if (primeiroSemestre.get(i).getTemperatura() > media)
+            if (primeiroSemestre.get(i).getTemperatura() > media) // se um mes de list tive uma temperatura maior que a média, imprima... 
             {
 
                 System.out.println(
@@ -54,13 +54,13 @@ public class Temperatura {
 }
 
 
-class Mes {
+class Mes { // classe criada para integrar os atributos de list (classe abstrata).
 
     private Integer ordem;
     private String nome;
     private Integer temperatura;
 
-    public Mes(Integer ordem, String nome, Integer temperatura){
+    public Mes(Integer ordem, String nome, Integer temperatura){ // método de construção. 
 
         this.ordem = ordem;
         this.nome = nome;
@@ -68,38 +68,40 @@ class Mes {
 
     }
 
-    public Integer getOrdem() 
+    public Integer getOrdem() // Integer devolve um inteiro para getOrdem
     {
 
-        return ordem;
+        return ordem; //Retorne 'ordem' (um inteiro) para getOrdem.
 
     }
 
-    public String getNome() 
+    public String getNome() //String devolve um texto para getNome
     {
 
-        return nome;
+        return nome; // Retorne 'nome'(um string) para getNome. 
 
     }
 
-    public Integer getTemperatura() 
+    public Integer getTemperatura() // Integer devolve um inteiro para getTemperatura
     {
 
-        return temperatura;
+        return temperatura; //Retorne 'temperatura'(um inteiro) para getTemperatura.
 
     }
 
     @Override
-    public String toString() 
+    public String toString() //Método de sobrescrição.
     {
 
         return "{" + 
-                " Ordem = " + ordem + '\'' + 
+                " Ordem = " + ordem + 
+                '\'' + 
                 ", Nome = " + nome + 
                 ", Temperatura = " + temperatura + '\'' +
                 '}';
 
     }
+    
 }
 
 
